e9e8c8da77b7db5c9deafd7c1661d9b5d2b0fafa
xxdd <xjb_1990@163.com>
2018-03-22 23:09:58 +0800

    引人Snapkit，简单实用自动布局

--- FirstSwiftProject/ViewController.swift
+++ FirstSwiftProject/ViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var box = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let label = UILabel.init();
+        label.text = "text";
+        label.textColor = UIColor.red;
+        label.font = UIFont.systemFont(ofSize: 18);
+        self.view.addSubview(label);
+        label.snp.makeConstraints { (make) -> Void in
+            make.centerX.equalToSuperview()
+            make.width.equalTo(100)
+            make.height.equalTo(20)
+            make.top.equalTo(self.view).offset(40)
+        }
+        
+        let button = UIButton.init(type: UIButtonType.custom)
+        button.setTitle("action", for: UIControlState.normal)
+        button.backgroundColor = UIColor.purple
+        self.view.addSubview(button)
+        button.snp.makeConstraints { (make) -> Void in
+            make.width.height.equalTo(50)
+            make.left.equalTo(self.view).offset(80)
+            make.top.equalTo(label.snp.bottom).offset(50)
+        }
+        
+        box.backgroundColor = UIColor.blue
+        self.view.addSubview(box)
+        box.snp.makeConstraints { (make) -> Void in
+            make.width.equalTo(60)
+            make.height.equalTo(20)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(button.snp.bottom).offset(70)
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+source 'https://github.com/CocoaPods/Specs.git'
+target 'FirstSwiftProject' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'SnapKit', '~> 3.2.0'
+  # Pods for FirstSwiftProject
+
+  target 'FirstSwiftProjectTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FirstSwiftProjectUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
